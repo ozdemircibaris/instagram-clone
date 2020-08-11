b7537bb8124aa4448da7973e619e802242ceb02f
Barış ÖZDEMİRCİ <ozdemircibaris@gmail.com>
2020-08-11 10:05:03 +0300

    Initial Commit

---
+++ instagram-clone/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  instagram-clone
+//
+//  Created by Baris Ozdemirci on 11.08.2020.
+//  Copyright © 2020 Barış ÖZDEMİRCİ. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
